591754a2352e1faae127913fff65d2cc79bbef27
thinkhappily <thinkhappily@gmail.com>
2016-10-17 22:10:07 +0800

    Removed unnessary Application class annotations

--- src/main/java/com/springbootdemo/Application.java
+++ src/main/java/com/springbootdemo/Application.java
@@ -8,8 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableWebMvc
 public class Application extends SpringBootServletInitializer
 {
     public static void main(String[] args)
